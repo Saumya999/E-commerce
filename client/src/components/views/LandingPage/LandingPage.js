2a6eb45fd72169cb370aec80ad0604386de5adc5
Saumya999 <saumyadiptasarkar49@gmail.com>
2020-07-28 15:37:46 +0530

    Fixing the linear issue

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -144,7 +144,7 @@ function LandingPage() {
     return (
         <div style={{ width: '75%', margin: '3rem auto' }}>
             <div style={{ textAlign: 'center' }}>
-                <h2>  Let's Travel Anywhere  <Icon type="rocket" />  </h2>
+                <h2>  Let's Shop  <Icon type="shop" />  </h2>
             </div>
 
 
